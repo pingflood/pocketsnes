8e67ce0c9a492010d773919b1ee6035c8216bb3c
SiENcE <sience@schattenkind.net>
2014-05-06 22:59:09 +0000

    Import "+support for SDD1 Emu added (GraphicPack not needed anymore!!)".
    This commit is based on https://github.com/dmitrysmagin/snes9x4d-rzx50/commit/c1b5ea12c53c0c3b1ae083f7e999b8c6c7e010bd but includes only the code relevant to S-DD1 decompression chip emulation.

--- pocketsnes/include/sdd1.h
+++ pocketsnes/include/sdd1.h
@@ -92,7 +92,5 @@
 void S9xSetSDD1MemoryMap (uint32 bank, uint32 value);
 void S9xResetSDD1 ();
 void S9xSDD1PostLoadState ();
-void S9xSDD1SaveLoggedData ();
-void S9xSDD1LoadLoggedData ();
 #endif
 
