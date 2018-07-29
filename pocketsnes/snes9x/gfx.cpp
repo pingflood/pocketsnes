babc18490b04f9808ae45550f12942c96e5ddf82
pingflood <pingflood@gmail.com>
2018-07-29 02:01:56 -0300

    Fix missing (.

--- pocketsnes/snes9x/gfx.cpp
+++ pocketsnes/snes9x/gfx.cpp
@@ -3720,8 +3720,7 @@ void S9xUpdateScreen ()
 		}
         // BJ: And we have to change the height if Interlace gets set,
         //     too.
-		if (IPPU.Interlace && !IPPU.DoubleHeightPixels)
-		{
+		if (IPPU.Interlace && !IPPU.DoubleHeightPixels) {
 			starty = GFX.StartY * 2;
 			endy = GFX.EndY * 2 + 1;
             IPPU.RenderedScreenHeight = PPU.ScreenHeight << 1;
